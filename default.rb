9305dd0b03bf698306a929bcb33e4d62d2832c28
root <root@chef-workstation.dexter.com.br>
2018-02-20 14:10:13 -0300

    Configuracao Base de Usuarios, Pacotes e Hosts

---
+++ default.rb
@@ -0,0 +1,4 @@
+include_recipe 'confsbase-chef::users'
+include_recipe 'confsbase-chef::debian-install'
+include_recipe 'confsbase-chef::redhat-install'
+include_recipe 'confsbase-chef::hosts'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'confsbase-4linuxdevops'
+maintainer '4Linux DevOps'
+maintainer_email 'seunome@seudominio'
+license 'All Rights Reserved'
+description 'Configuracao base'
+long_description 'Configuracao base de usuarios, pacotes e hosts'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
