e66379141e8a7347a25af11ea565d66e77351cbd
4linuxdevops <35940131+4linuxdevops@users.noreply.github.com>
2018-03-16 19:40:34 -0300

    Delete debian-install.rb

--- debian-install.rb
+++
@@ -1,4 +0,0 @@
-package ['tzdata', 'dnsutils', 'bash-completion', 'unzip', 'bzip2', 'htop', 'vim', 'mlocate', 'nfs-common'] do
-  action :install
-  only_if { node['platform_family'] == 'debian' }
-end
